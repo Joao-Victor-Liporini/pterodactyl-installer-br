c082ee28c33b05ac61cdc4d0298dfef8a62d0fdd
João Victor <81539403+Joao-Victor-Liporini@users.noreply.github.com>
2023-03-16 23:21:32 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,42 +2,44 @@
 
 set -e
 
-######################################################################################
-#                                                                                    #
-# Project 'pterodactyl-installer'                                                    #
-#                                                                                    #
-# Copyright (C) 2018 - 2023, Vilhelm Prytz, <vilhelm@prytznet.se>                    #
-#                                                                                    #
-#   This program is free software: you can redistribute it and/or modify             #
-#   it under the terms of the GNU General Public License as published by             #
-#   the Free Software Foundation, either version 3 of the License, or                #
-#   (at your option) any later version.                                              #
-#                                                                                    #
-#   This program is distributed in the hope that it will be useful,                  #
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of                   #
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                    #
-#   GNU General Public License for more details.                                     #
-#                                                                                    #
-#   You should have received a copy of the GNU General Public License                #
-#   along with this program.  If not, see <https://www.gnu.org/licenses/>.           #
-#                                                                                    #
-# https://github.com/pterodactyl-installer/pterodactyl-installer/blob/master/LICENSE #
-#                                                                                    #
-# This script is not associated with the official Pterodactyl Project.               #
-# https://github.com/pterodactyl-installer/pterodactyl-installer                     #
-#                                                                                    #
-######################################################################################
-
-export GITHUB_SOURCE="v0.12.3"
-export SCRIPT_RELEASE="v0.12.3"
-export GITHUB_BASE_URL="https://raw.githubusercontent.com/pterodactyl-installer/pterodactyl-installer"
+######################################################################################## 
+#                                                                                      #
+# Projeto 'pterodactyl-installer-br'                                                   #
+#                                                                                      #
+# Copyright (C) 2018 - 2023, Vilhelm Prytz, <vilhelm@prytznet.se>                      #
+#                                                                                      #
+#   Este programa é software livre: pode redistribuí-lo e/ou modificá-lo               #
+#   nos termos da Licença Pública Geral GNU, tal como publicada por                    #
+#   Free Software Foundation, requer a versão 3 da Licença, requer                     #
+#   (à sua escolha) qualquer versão posterior.                                         #
+#                                                                                      #
+#   Este programa é distribuído na esperança de que venha a ser útil,                  #
+#   mas SEM QUALQUER GARANTIA; sem sequer a garantia implícita de                      #
+#   MERCANTABILIDADE ou ADEQUAÇÃO PARA UM FINAL PARTICULAR. Veja o                     #
+#   GNU General Public License para mais detalhes.                                     #
+#                                                                                      #
+#   Você deverá ter recebido uma cópia da Licença Pública Geral GNU                    #
+#   juntamente com este programa.  Caso contrário, veja                                #
+#   <https://www.gnu.org/licenses/>.                                                   #
+#                                                                                      #
+# https://github.com/Joao-Victor-Liporini/pterodactyl-installer-br/blob/master/LICENSE #
+#                                                                                      #
+# Este script não está associado ao Projecto oficial Pterodactyl-BR, Nem mesmo ao      #
+# Projeto oficial Pterodactyl                                                          #
+# https://github.com/Next-Panel/Pterodactyl-BR                                         #
+# https://github.com/pterodactyl/panel                                                 #
+#                                                                                      #
+########################################################################################
+export GITHUB_SOURCE="v0.13.0"
+export SCRIPT_RELEASE="v0.13.0"
+export GITHUB_BASE_URL="https://raw.githubusercontent.com/Joao-Victor-Liporini/pterodactyl-installer-br/"
 
 LOG_PATH="/var/log/pterodactyl-installer.log"
 
 # check for curl
 if ! [ -x "$(command -v curl)" ]; then
-  echo "* curl is required in order for this script to work."
-  echo "* install using apt (Debian and derivatives) or yum/dnf (CentOS)"
+  echo "* é necessário curl para que este script funcione."
+  echo "* instale usando apt (Debian e derivados) ou yum/dnf (CentOS)"
   exit 1
 fi
 
@@ -48,19 +50,19 @@ curl -sSL -o /tmp/lib.sh "$GITHUB_BASE_URL"/"$GITHUB_SOURCE"/lib/lib.sh
 source /tmp/lib.sh
 
 execute() {
-  echo -e "\n\n* pterodactyl-installer $(date) \n\n" >>$LOG_PATH
+  echo -e "\n\n* pterodactyl-installer-br $(date) \n\n" >>$LOG_PATH
 
   [[ "$1" == *"canary"* ]] && export GITHUB_SOURCE="master" && export SCRIPT_RELEASE="canary"
   update_lib_source
   run_ui "${1//_canary/}" |& tee -a $LOG_PATH
 
   if [[ -n $2 ]]; then
-    echo -e -n "* Installation of $1 completed. Do you want to proceed to $2 installation? (y/N): "
+    echo -e -n "* Instalação de $1 concluído. Deseja proceder à instalação de $2? (s/N): "
     read -r CONFIRM
-    if [[ "$CONFIRM" =~ [Yy] ]]; then
+    if [[ "$CONFIRM" =~ [Ss] ]]; then
       execute "$2"
     else
-      error "Installation of $2 aborted."
+      error "Instalação de $2 abortada."
       exit 1
     fi
   fi
@@ -71,42 +73,42 @@ welcome ""
 done=false
 while [ "$done" == false ]; do
   options=(
-    "Install the panel"
-    "Install Wings"
-    "Install both [0] and [1] on the same machine (wings script runs after panel)"
-    # "Uninstall panel or wings\n"
+    "Instalar o painel"
+    "Instalar o Wings"
+    "Instalar ambos [0] e [1] na mesma máquina (o roteiro das asas corre depois do painel)"
+    "Desinstalar painel ou wings"
 
-    "Install panel with canary version of the script (the versions that lives in master, may be broken!)"
-    "Install Wings with canary version of the script (the versions that lives in master, may be broken!)"
-    "Install both [3] and [4] on the same machine (wings script runs after panel)"
-    "Uninstall panel or wings with canary version of the script (the versions that lives in master, may be broken!)"
+    "Instalar o Painel com versão canária do script (as versões que vivem em mestre, podem ser quebradas!)"
+    "Instalar o Wings com versão canária do script (as versões que vivem em mestre, podem ser quebradas!)"
+    "Instalar ambos [3] e [4] na mesma máquina (o roteiro das asas corre depois do painel)"
+    "Desinstalar painel ou asas com versão canária do guião (as versões que vivem em mestre, podem ser quebradas!)"
   )
 
   actions=(
-    "panel"
+    "painel"
     "wings"
-    "panel;wings"
-    # "uninstall"
+    "painel;wings"
+    "desinstalar"
 
-    "panel_canary"
+    "painel_canary"
     "wings_canary"
-    "panel_canary;wings_canary"
-    "uninstall_canary"
+    "painel_canary;wings_canary"
+    "desinstalar_canary"
   )
 
-  output "What would you like to do?"
+  output "O que gostaria de fazer?"
 
   for i in "${!options[@]}"; do
     output "[$i] ${options[$i]}"
   done
 
-  echo -n "* Input 0-$((${#actions[@]} - 1)): "
+  echo -n "* Insira uma opção de 0 a $((${#actions[@]} - 1)): "
   read -r action
 
-  [ -z "$action" ] && error "Input is required" && continue
+  [ -z "$action" ] && error "É necessário inserir uma opção" && continue
 
   valid_input=("$(for ((i = 0; i <= ${#actions[@]} - 1; i += 1)); do echo "${i}"; done)")
-  [[ ! " ${valid_input[*]} " =~ ${action} ]] && error "Invalid option"
+  [[ ! " ${valid_input[*]} " =~ ${action} ]] && error "Opção inválida"
   [[ " ${valid_input[*]} " =~ ${action} ]] && done=true && IFS=";" read -r i1 i2 <<<"${actions[$action]}" && execute "$i1" "$i2"
 done
 
