da1cbd722874cfda5731be50543ffb50f9fcbdf1
João Victor <81539403+Joao-Victor-Liporini@users.noreply.github.com>
2023-03-17 18:53:00 -0300

    Update biblioteca.sh

--- lib/biblioteca.sh
+++ lib/biblioteca.sh
@@ -55,7 +55,7 @@ export SUPPORTED=false
 export PANEL_DL_URL="https://github.com/Next-Panel/Pterodactyl-BR/releases/download/latest/panel.tar.gz"
 export WINGS_DL_BASE_URL="https://github.com/pterodactyl/wings/releases/latest/download/wings_linux_"
 export MARIADB_URL="https://downloads.mariadb.com/MariaDB/mariadb_repo_setup"
-export GITHUB_BASE_URL=${GITHUB_BASE_URL:-"https://github.com/Joao-Victor-Liporini/pterodactyl-installer-br"}
+export GITHUB_BASE_URL=${GITHUB_BASE_URL:-"https://raw.githubusercontent.com/Joao-Victor-Liporini/pterodactyl-installer-br"}
 export GITHUB_URL="$GITHUB_BASE_URL/$GITHUB_SOURCE"
 
 # Colors
