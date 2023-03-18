d2c834319126280c94e52e2ef89a8f830ab4746b
João Victor <81539403+Joao-Victor-Liporini@users.noreply.github.com>
2023-03-17 23:08:56 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -30,7 +30,7 @@ set -e
 
 export GITHUB_SOURCE="v0.12.3"
 export SCRIPT_RELEASE="v0.12.3"
-export GITHUB_BASE_URL="https://raw.githubusercontent.com/Joao-Victor-Liporini/pterodactyl-installer-br/tree/installer-us"
+export GITHUB_BASE_URL="https://raw.githubusercontent.com/Joao-Victor-Liporini/pterodactyl-installer-br/installer-us"
 
 LOG_PATH="/var/log/pterodactyl-installer.log"
 
